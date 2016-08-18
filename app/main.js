7e294710f2a21869718ea8badd05f3338c80bb05
andela-mkwamboka <monicah.kwamboka@andela.com>
2016-08-18 11:51:56 +0300

    [Feature/#128491295] set up react-router

--- app/main.js
+++ app/main.js
@@ -1,1 +1,12 @@
-require('./style.scss');
+import React from 'react';
+import { Router, Route, hashHistory } from 'react-router';
+import { render } from 'react-dom';
+import Home from './components/Home';
+import About from './components/pages/About';
+
+render((
+  <Router history={hashHistory}>
+    <Route component={Home} path='/'/>
+    <Route component={About} path='/about'/>
+  </Router>
+), document.getElementById('id'));
