c337be4c2edb8561e887b45f82ce724d175bca7b
Jacky <jackline.kimani@andela.com>
2016-08-24 16:13:36 +0300

    [Feature #128396243] Add initial user

--- app/store/configureStore.js
+++ app/store/configureStore.js
@@ -1,9 +1,17 @@
-import {createStore, applyMiddleware} from "redux";
-import rootReducer from "../reducers";
+import {createStore} from 'redux';
+import rootReducer from '../reducers/index.js';
 
-export default function configureStore(initialState) {
+const initialState = {
+	creds: {
+		name: '',
+		email: ''
+	}
+};
+
+
+const store = function configureStore(initialState) {
 	return createStore (
 		rootReducer,
 		initialState
 		);
-}
+};
