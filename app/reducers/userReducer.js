fd237888d1e0b748675ce06fb5e3a80acee1afd7
Jacky <jackline.kimani@andela.com>
2016-08-24 16:16:45 +0300

    [Feature #128396243] Add login reducers

---
+++ app/reducers/userReducer.js
@@ -0,0 +1,32 @@
+import {LOGIN_REQUEST, LOGIN_SUCCESS, LOGIN_FAILURE} from "./../constants/actionTypes";
+import logInUser from "./../actions/UserActions";
+
+const user = function auth(action, state = {
+	isFetching: false,
+	isAuthenticated: localStorage.getItem("id_token") ? true : false
+}) {
+	switch (action.type) {
+		case LOGIN_REQUEST:
+			return Object.assign({}, state, {
+				isFetching: true,
+				isAuthenticated: false,
+				user: user.action.creds
+			});
+		case LOGIN_SUCCESS:
+			return Object.assign({}, state, {
+				isFetching: false,
+				isAuthenticated: true,
+				errorMessage: ''
+			});
+		case LOGIN_FAILURE:
+			return Object.assign({}, state, {
+				isFetching: false,
+				isAuthenticated: false,
+				errorMessage: action.message
+			});
+		default:
+			return state;
+	}
+};
+
+export default user;
