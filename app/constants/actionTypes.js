0eaf80b005139003dd0cb58eabeb6d0cf3abd217
Jacky <jackline.kimani@andela.com>
2016-08-29 12:07:12 +0300

    [Feature #128396243] Remove LOGOUT constant

--- app/constants/actionTypes.js
+++ app/constants/actionTypes.js
@@ -6,7 +6,6 @@ export const LOGOUT_REQUEST = 'LOGOUT_REQUEST';
 export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS';
 export const LOGOUT_FAILURE = 'LOGOUT_FAILURE';
 
-export const LOGOUT = 'LOGOUT';
 export const SIGNUP = 'SIGNUP';
 export const SESSION = 'SESSION';
 export const FIND = 'FIND';
