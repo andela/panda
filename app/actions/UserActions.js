08d71e17c85b4a67745186019d22f1bc504e081f
Jacky <jackline.kimani@andela.com>
2016-08-29 11:00:06 +0300

    [Feature #128396243] Change action types from string

--- app/actions/UserActions.js
+++ app/actions/UserActions.js
@@ -1,8 +1,9 @@
 import { LOGIN_REQUEST, LOGIN_SUCCESS, LOGIN_FAILURE } from './../constants/actionTypes';
+import request from 'superagent';
 
 function logInRequest(creds) {
 	return {
-		type: 'LOGIN_REQUEST',
+		type: LOGIN_REQUEST,
 		isFetching: true,
 		isAuthenticated: false,
 		creds
@@ -11,7 +12,7 @@ function logInRequest(creds) {
 
 function receiveLogIn(user) {
 	return {
-		type: 'LOGIN_SUCCESS',
+		type: LOGIN_SUCCESS,
 		isFetching: false,
 		isAuthenticated: true,
 		id_token: user.id_token
@@ -20,7 +21,7 @@ function receiveLogIn(user) {
 
 function logInError(message) {
 	return {
-		type: 'LOGIN_FAILURE',
+		type: LOGIN_FAILURE,
 		isFetching: false,
 		isAuthenticated: false,
 		message
@@ -29,11 +30,6 @@ function logInError(message) {
 
 // calls the API to get atoken and dispatches actions along the way
 export function logInUser(creds) {
-	let config = {
-		method: 'POST',
-		headers: { 'Content-Type':'application/x-www-form-urlencoded' },
-		body: `name=${creds.name}&email=${creds.email}`
-	};
 
 	return dispatch => {
 		// kickOff the call to the API
@@ -44,13 +40,13 @@ export function logInUser(creds) {
 			.set('Content-Type', 'application/x-www-form-urlencoded')
 			.send({name:creds.name, email:creds.email})
 			.end(function(err, response) {
+				var user = response.body;
 				if (!response.ok || err) {
-					dispatch(logInError(user.message));
+					dispatch(logInError(err));
 				} else {
-					localStorage.setItem('id_token', user.id_token);
+					localStorage.setItem('id_token', user);
 					dispatch(receiveLogIn(user));
 				}
 			});
 	};
 }
-
