cb857add6a5625a3e5bb42ebd0d13dc04a9c1efe
Jacky <jackline.kimani@andela.com>
2016-08-29 12:04:55 +0300

    [Feature #128396243] Add logout reducers

--- app/reducers/userReducer.js
+++ app/reducers/userReducer.js
@@ -1,11 +1,13 @@
-import {LOGIN_REQUEST, LOGIN_SUCCESS, LOGIN_FAILURE} from './../constants/actionTypes';
-
+import {LOGIN_REQUEST, LOGIN_SUCCESS, LOGIN_FAILURE, LOGOUT_REQUEST, LOGOUT_SUCCESS, LOGOUT_FAILURE } from './../constants/actionTypes';
 
 const user = function auth(action, state = {
 	isFetching: false,
 	isAuthenticated: localStorage.getItem('id_token') ? true : false
 }) {
+
 	switch (action.type) {
+
+		//logging in
 		case LOGIN_REQUEST:
 			return Object.assign({}, state, {
 				isFetching: true,
@@ -24,6 +26,27 @@ const user = function auth(action, state = {
 				isAuthenticated: false,
 				errorMessage: action.message
 			});
+
+		// logging out
+		case LOGOUT_REQUEST:
+			return Object.assign({}, state, {
+				isFetching: true,
+				isAuthenticated: true,
+				user: action.cred
+			});
+		case LOGOUT_SUCCESS:
+			return Object.assign({}, state, {
+				isFetching: false,
+				isAuthenticated: false,
+				errorMessage: ''
+			});
+		case LOGOUT_FAILURE:
+			return Object.assign({}, state, {
+				isFetching: false,
+				isAuthenticated: false,
+				errorMessage: action.message
+			});
+
 		default:
 			return state;
 	}
