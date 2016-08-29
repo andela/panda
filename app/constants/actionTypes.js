0320a507004f46b18565e38f1d97222ea3dc89b3
Jacky <jackline.kimani@andela.com>
2016-08-29 11:01:07 +0300

    [Feature #128396243] Change double quotes to single quotes

--- app/constants/actionTypes.js
+++ app/constants/actionTypes.js
@@ -1,8 +1,8 @@
-export const LOGIN_REQUEST = "LOGIN_REQUEST";
-export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
-export const LOGIN_FAILURE = "LOGIN_FAILURE";
-export const LOGOUT = "LOGOUT";
-export const SIGNUP = "SIGNUP";
-export const SESSION = "SESSION";
-export const FIND = "FIND";
-export const UPDATE = "UPDATE";
+export const LOGIN_REQUEST = 'LOGIN_REQUEST';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAILURE = 'LOGIN_FAILURE';
+export const LOGOUT = 'LOGOUT';
+export const SIGNUP = 'SIGNUP';
+export const SESSION = 'SESSION';
+export const FIND = 'FIND';
+export const UPDATE = 'UPDATE';
